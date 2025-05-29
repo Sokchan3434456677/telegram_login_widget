39df25e6bbe111c58d1e9613451e2684599b18a3
Earsokchan <145833822+Sokchan3434456677@users.noreply.github.com>
2025-05-29 22:53:53 +0700

    Update api.js

--- Clinet/src/utils/api.js
+++ Clinet/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://apiwidget-v1.vercel.app';
 
 export const checkAuth = async () => {
   try {
